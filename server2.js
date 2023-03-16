29619a0eda106068b85da1ee18d5b2d61391ea57
Asus <tungktph27526@fpt.edu.vn>
2023-03-16 22:40:41 +0700

    first commit

---
+++ server2.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var expressHbs = require('express-handlebars');
+app.engine('.hbs', expressHbs.engine({exitname: 'hbs',defaultLayout:'',layoutDir:''}));
+app.set('view engine','.hbs');
+app.get('/', function(req, res){
+    res.send('hello world');
+})
+
+app.get('/baiTap', function(req, res){
+    res.render('index2');
+})
+app.listen(process.env.PORT || '3000')
